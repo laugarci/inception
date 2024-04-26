1584f1342e99f298ac9e45c92b13d4369820f562
root <garch.laura@gmail.com>
2024-04-26 13:07:08 +0200

    inception

--- srcs/requirements/nginx/tools/nginx_www.sh
+++ srcs/requirements/nginx/tools/nginx_www.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 if [ ! -f /etc/ssl/laugarci.42.fr.crt ]; then
-openssl req -newkey rsa:4096 -x509 -days 365 -nodes -out /etc/ssl/laugarci.42.fr.crt -keyout /etc/ssl/laugarci.42.fr.key -subj "/C=ES/ST=Barcelona/L=Barcelona/O=42 Barcelona/CN=laugarci.42.fr";
+	openssl req -newkey rsa:4096 -x509 -days 365 -nodes -out /etc/ssl/laugarci.42.fr.crt -keyout /etc/ssl/laugarci.42.fr.key -subj "/C=ES/ST=Barcelona/L=Barcelona/O=42 Barcelona/CN=laugarci.42.fr";
 fi
 exec "$@"
